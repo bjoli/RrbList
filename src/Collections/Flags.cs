51de716c1f543249292e17ad325fc3e22695cd98
linus björnstam <linus.internet@fastmail.se>
2025-12-16 10:11:22 +0100

    Added flags for future use
    Using flags and a counter means we can reduce the size of a node by 8 bytes.

---
+++ src/Collections/Flags.cs
@@ -0,0 +1,17 @@
+namespace Collections;
+
+[Flags]
+internal enum NodeFlags : byte
+{
+    // Bits 3-8 are currently not in use.
+    None = 0,
+    IsRelaxed = 1 << 0,     // 1
+    IsLeaf = 1 << 1,        // 2
+    IsFrozen = 1 << 2,      // 4
+    IsMediumRare = 1 << 3,  // 8
+    IsPineapple = 1 << 4,   // 16
+    Tyrannosaurus = 1 << 5, // 32
+    MotherNode = 1 << 6,    // 64
+    CheckMate = 1 << 7,     // 128
+    
+}
